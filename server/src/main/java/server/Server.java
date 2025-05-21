170da537ca8c4b59494f1c07c88108d0fd057077
Hunter Winterton <hmwinterton@hotmail.com>
2025-05-20 22:29:18 -0600

    Modify Server, 19/22 tests pass

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,14 @@
 package server;
 
-import spark.*;
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import handler.ClearHandler;
+import handler.GameHandler;
+import handler.UserHandler;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
+import spark.Spark;
 
 public class Server {
 
@@ -11,8 +19,31 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
+        //This line initializes the server and can be removed once you have a functioning endpoint
         Spark.init();
+
+        DataAccess db = new MemoryDataAccess();
+        UserService userService = new UserService(db);
+        GameService gameService = new GameService(db);
+        ClearService clearService = new ClearService(db);
+
+        // Initialize handlers
+        UserHandler userHandler = new UserHandler(userService);
+        GameHandler gameHandler = new GameHandler(gameService);
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
+        // Register user endpoints
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
+
+        // Register game endpoints
+        Spark.post("/game", gameHandler::createGame);
+        Spark.get("/game", gameHandler::listGames);
+        Spark.put("/game", gameHandler::joinGame);
+
+        // Register clear endpoint
+        Spark.delete("/db", clearHandler::clear);
 
         Spark.awaitInitialization();
         return Spark.port();
