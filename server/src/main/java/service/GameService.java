20bccbab10e48b7e9455a23dc95d8f08ca669c57
Hunter Winterton <hmwinterton@hotmail.com>
2025-05-20 19:06:58 -0600

    GameService created

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,61 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+
+import java.util.Collection;
+
+public class GameService {
+    private final DataAccess db;
+
+    public GameService(DataAccess db) {
+        this.db = db;
+    }
+
+    public int createGame(String authToken, String gameName) throws DataAccessException {
+        validateAuth(authToken);
+        if (gameName == null || gameName.isEmpty()) {
+            throw new DataAccessException("Missing game name");
+        }
+
+        GameData newGame = new GameData(0, null, null, gameName, new ChessGame());
+        return db.createGame(newGame);
+    }
+
+    public Collection<GameData> listGames(String authToken) throws DataAccessException {
+        validateAuth(authToken);
+        return db.listGames();
+    }
+
+    public void joinGame(String authToken, int gameID, String playerColor) throws DataAccessException {
+        AuthData auth = validateAuth(authToken);
+        GameData game = db.getGame(gameID);
+
+        if (game == null) {
+            throw new DataAccessException("Game not found");
+        }
+
+        String username = auth.username();
+        String white = game.whiteUsername();
+        String black = game.blackUsername();
+
+        if ("WHITE".equalsIgnoreCase(playerColor)) {
+            if (white != null) throw new DataAccessException("Color already taken");
+            db.updateGame(new GameData(gameID, username, black, game.gameName(), game.game()));
+        } else if ("BLACK".equalsIgnoreCase(playerColor)) {
+            if (black != null) throw new DataAccessException("Color already taken");
+            db.updateGame(new GameData(gameID, white, username, game.gameName(), game.game()));
+        } else {
+            throw new DataAccessException("Invalid color");
+        }
+    }
+
+    private AuthData validateAuth(String token) throws DataAccessException {
+        AuthData auth = db.getAuth(token);
+        if (auth == null) throw new DataAccessException("Unauthorized");
+        return auth;
+    }
+}
