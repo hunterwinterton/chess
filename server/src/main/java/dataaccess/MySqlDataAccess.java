7cd47c571cdff87188c2166a98e4c38aa6ab66cd
Hunter Winterton <hmwinterton@hotmail.com>
2025-05-26 12:25:15 -0600

    user data access

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,6 +1,11 @@
 package dataaccess;
 
 import com.google.gson.Gson;
+import model.AuthData;
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
 
 public class MySqlDataAccess implements DataAccess {
     private final Gson gson = new Gson();
@@ -41,5 +46,63 @@ public class MySqlDataAccess implements DataAccess {
     }
 
     private void executeUpdate(String statement, Object... params) {
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        String hashed = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        executeUpdate(
+                "INSERT INTO user(username,password,email) VALUES(?,?,?)",
+                user.username(), hashed, user.email()
+        );
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        String sql = "SELECT username,password,email FROM user WHERE username=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, username);
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new UserData(
+                            rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return null;
+    }
+
+    public void createAuth(AuthData auth) throws DataAccessException {
+        executeUpdate(
+                "INSERT INTO auth(token,username) VALUES(?,?)",
+                auth.authToken(), auth.username()
+        );
+    }
+
+    public AuthData getAuth(String token) throws DataAccessException {
+        String sql = "SELECT token,username FROM auth WHERE token=?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, token);
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new AuthData(
+                            rs.getString("token"),
+                            rs.getString("username")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return null;
+    }
+
+    public void deleteAuth(String token) throws DataAccessException {
+        executeUpdate("DELETE FROM auth WHERE token=?", token);
     }
 }
