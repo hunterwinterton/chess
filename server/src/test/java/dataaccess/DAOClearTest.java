3d74471746bdd9c9d42b1c12f8d1c72afcfd45e6
Hunter Winterton <hmwinterton@hotmail.com>
2025-05-27 16:35:19 -0600

    clear dao test

---
+++ server/src/test/java/dataaccess/DAOClearTest.java
@@ -0,0 +1,38 @@
+package dataaccess;
+
+import model.UserData;
+import model.AuthData;
+import model.GameData;
+import chess.ChessGame;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DAOClearTest {
+    private DataAccess dao;
+
+    @BeforeAll
+    void setup() throws Exception {
+        dao = new MySqlDataAccess();
+    }
+
+    @Test
+    void clear_removesAllData() throws DataAccessException {
+
+        dao.createUser(new UserData("hunter", "pass", "hunter@email.com"));
+        dao.createUser(new UserData("winter", "pass", "winter@email.com"));
+
+        dao.createAuth(new AuthData("tokenH", "hunter"));
+        dao.createAuth(new AuthData("tokenW", "winter"));
+
+        dao.createGame(new GameData(0, "hunter", "winter", "TestGame", new ChessGame()));
+
+        dao.clear();
+
+        assertNull(dao.getUser("hunter"));
+        assertNull(dao.getUser("winter"));
+        assertNull(dao.getAuth("tokenH"));
+        assertNull(dao.getAuth("tokenW"));
+        assertTrue(dao.listGames().isEmpty());
+    }
+}
