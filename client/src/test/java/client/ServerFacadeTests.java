42c24f728e8170064b70a186b17607389d632877
Hunter Winterton <hmwinterton@hotmail.com>
2025-06-04 18:35:48 -0600

    games tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -77,4 +77,54 @@ public class ServerFacadeTests {
         );
     }
 
+    @Test
+    void createGamePositive() throws Exception {
+        AuthData auth = facade.register("hunter4", "password", "hunter4@example.com");
+        facade.setAuthToken(auth.authToken());
+        int gameId = facade.createGame("game1");
+        assertTrue(gameId > 0);
+    }
+
+    @Test
+    void createGameNegative_notLoggedIn() {
+        assertThrows(Exception.class, () ->
+                facade.createGame("game1")
+        );
+    }
+
+    @Test
+    public void listGamesPositive() throws Exception {
+        AuthData a5 = facade.register("hunter5", "hunterpass5", "h5@example.com");
+        facade.setAuthToken(a5.authToken());
+        facade.createGame("onlyGame");
+        assertNotNull(facade.listGames());
+    }
+
+    @Test
+    void listGamesNegative_notLoggedIn() {
+        assertThrows(Exception.class, () ->
+                facade.listGames()
+        );
+    }
+
+    @Test
+    void joinGamePositive() throws Exception {
+        AuthData authA = facade.register("hunter6", "password", "hunter6@example.com");
+        facade.setAuthToken(authA.authToken());
+        int gameId = facade.createGame("game3");
+
+        AuthData authB = facade.register("hunter7", "password", "hunter7@example.com");
+        facade.setAuthToken(authB.authToken());
+        facade.joinGame(gameId, "WHITE");
+    }
+
+    @Test
+    void joinGameNegative_invalidGameId() throws Exception {
+        AuthData auth = facade.register("hunter8", "password", "hunter8@example.com");
+        facade.setAuthToken(auth.authToken());
+        assertThrows(Exception.class, () ->
+                facade.joinGame(9999, "WHITE")
+        );
+    }
+
 }
